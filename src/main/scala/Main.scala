e9a116f7111262940022b230c671578b7ceb3d6a
NKozlovskiy <nkozlovskiy@beeline.ru>
2021-10-13 14:11:05 +0300

    add assertions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,19 @@
 import RnpCalculator.{computeRpn, rpn}
 
 object Main extends App {
-  val b = "((1+2)*(3+(40/5)))".toCharArray.iterator
-  val rpnStr = rpn(b)
-  println(rpnStr mkString " ")
-  val result = computeRpn(rpnStr)
-  println(result)
+  val test = Map(
+    "(2+1)" -> 3.0,
+    "(2-1)" -> 1.0,
+    "(4*2)" -> 8.0,
+    "(4/2)" -> 2.0,
+    "(2*(1+2))" -> 6.0,
+    "((2*1)+2)" -> 4.0,
+    "((1+2)*(3+(4*5)))" -> 69.0,
+    "((1+2)*(3+(40/5)))" -> 33.0,
+    "((1020)*(3+(2-5)))" -> 0.0
+  )
+
+  for ((exp, result) <- test) {
+    assert(computeRpn(rpn(exp.toCharArray.iterator)) - result < 0.0000000000001)
+  }
 }
